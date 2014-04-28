6742a95c67d966c5f54bc97d79e792348bc49287
Anup Bishnoi <pixelsallover@gmail.com>
2014-04-28 20:12:58 +0530

    firebase plugin got API now, yeah!

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,61 +2,136 @@ var Firebase = require("firebase"),
     _ = require("underscore"),
     shouldbe = require("shouldbe");
 
-module.exports = function (firebasePath, entities, log) {
-    var db = new Firebase(firebasePath);
+module.exports = function (firebasePath, resourceNames, log) {
+    var firebaseRoot = new Firebase(firebasePath);
     
-    var data = {},
+    var db = { data: {}},
+        data = db.data,
         log = log || console.log.bind(console);
 
     shouldbe("string", firebasePath);
-    shouldbe("array", entities);
+    shouldbe("array", resourceNames);
     shouldbe("function", log);
     
-    entities.forEach(function (type) {
-        data[type] = {};
-        data[type].local = [];
-        data[type].names = [];
-        data[type].cloud = db.child(type);
+    resourceNames.forEach(function (resourceName) {
+        shouldbe("string", resourceName);
+        
+        data[resourceName] = {};
+        data[resourceName].values = [];
+        data[resourceName].ids = [];
+        data[resourceName].cloud = firebaseRoot.child(resourceName);
     
-        data[type].cloud.on("child_added", function (snap) {
+        data[resourceName].cloud.on("child_added", function (snap) {
             var remoteItem = snap.val(),
                 stringified = JSON.stringify(remoteItem),
-                localIndex = data[type].local.length;
+                localIndex = data[resourceName].values.length;
             
-            data[type].local[localIndex] = remoteItem;
-            data[type].names[localIndex] = snap.name();
+            data[resourceName].values[localIndex] = remoteItem;
+            data[resourceName].ids[localIndex] = snap.name();
             log("Added posts[" + localIndex + "]:\n" + stringified);
         });
         
-        data[type].cloud.on("child_removed", function (snap) {
+        data[resourceName].cloud.on("child_removed", function (snap) {
             var remoteItem = snap.val(),
                 stringified = JSON.stringify(remoteItem),
-                localNames = data[type].names,
+                localNames = data[resourceName].ids,
                 localItemIndex = localNames.indexOf(snap.name());
             
             if (~localItemIndex) {
-                data[type].local.splice(localItemIndex, 1);
+                data[resourceName].values.splice(localItemIndex, 1);
                 localNames.splice(localItemIndex, 1);
-                log("Removed " + type + "[" + localItemIndex + "]:\n" + stringified);
+                log("Removed " + resourceName + "[" + localItemIndex + "]:\n" + stringified);
             } else {
-                log("Removed " + type + " not in local: " + stringified);
+                log("Removed " + resourceName + " not in local values: " + stringified);
             }
         });
         
-        data[type].cloud.on("child_changed", function (snap) {
+        data[resourceName].cloud.on("child_changed", function (snap) {
             var remoteItem = snap.val(),
                 stringified = JSON.stringify(remoteItem),
-                localNames = data[type].names,
+                localNames = data[resourceName].ids,
                 localItemIndex = localNames.indexOf(snap.name());
     
             if (~localItemIndex) {
-                data[type].local[localItemIndex] = remoteItem;
-                log("Changed " + type + "[" + localItemIndex + "] to:\n" + stringified);
+                data[resourceName].values[localItemIndex] = remoteItem;
+                log("Changed " + resourceName + "[" + localItemIndex + "] to:\n" + stringified);
             } else {
-                log("Changed " + type + " doesn't exist in local: " + stringified);
+                log("Changed " + resourceName + " doesn't exist in local: " + stringified);
             }
         });
     });
     
-    return data;
+    db.read = function (resourceName, callback) {
+        shouldbe("string", resourceName);
+        shouldbe("object", data[resourceName]);
+        
+        var resources = data[resourceName].values;
+        callback(null, resources.map(function (resource, index) {
+            var id = data[resourceName].ids[index];
+            return _.extend(_.clone(resource), { id: id });
+        }));
+    };
+    
+    db.create = function (resourceName, resource, callback) {
+        shouldbe("string", resourceName);
+        shouldbe("object", data[resourceName]);
+        
+        var ref = data[resourceName].cloud.push(resource, function (err) {
+            if (err) return callback(err);
+            callback(null, _.extend(_.clone(resource), { id: ref.name() }));
+        });
+    };
+    
+    db.update = function (resourceName, resourceId, attributes, callback) {
+        shouldbe("string", resourceName);
+        shouldbe("object", data[resourceName]);
+        
+        var resourceInfo = getResource(data[resourceName], resourceId),
+            ref = resourceInfo.ref,
+            resource = resourceInfo.resource;
+        
+        if (!resource) return callback(new Error("Resource not found in database"));
+        
+        _.extend(resource, _.omit(attributes, "id")); // Can't update id
+        ref.set(resource, function (err) {
+            if (err) return callback(err);
+            callback(null, _.extend(_.clone(resource), { id: resourceId }));
+        });
+    };
+    
+    db.delete = function (resourceName, resourceId, callback) {
+        shouldbe("string", resourceName);
+        shouldbe("object", data[resourceName]);
+        
+        var resourceInfo = getResource(data[resourceName], resourceId),
+            ref = resourceInfo.ref,
+            resource = resourceInfo.resource;
+            
+        if (!resource) return callback(new Error("Resource not found in database"));
+        
+        ref.remove(function (err) {
+            if (err) return callback(err);
+            callback(null, _.extend(_.clone(resource), { id: resourceId }));
+        });
+    };
+    
+    function getResource(resources, resourceId) {
+        shouldbe("object", resources);
+        shouldbe("array", resources.ids);
+        shouldbe("array", resources.values);
+        shouldbe("object", resources.cloud);
+        
+        var index = resources.ids.indexOf(resourceId),
+            resource = resources.values[index];
+        
+        if (!resource) return {};
+        
+        return {
+            ref: resources.cloud.child(resourceId),
+            resource: resource
+        };
+    }
+
+    return db;
 };
+
